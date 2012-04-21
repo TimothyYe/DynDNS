17fd896625b89843b231db009c6ce79450592d77
Timothy <yexiaozhou2003@gmail.com>
2012-04-21 16:14:29 +0800

    Split logic into multi source files.

---
+++ DNSPodHelper.rb
@@ -0,0 +1,94 @@
+require 'singleton'
+require 'net/http'
+require 'net/https'
+require 'open-uri'
+require 'json'
+
+class DNSPodHelper
+
+  $login_email = ""
+  $login_password = ""
+
+  $format = "json"
+  $lang = "en"
+
+  include Singleton
+
+  def SetUserInfo(email, pass)
+      $login_email =  email
+      $login_password = pass
+  end
+
+  $userAgent = "DynDNS/0.1 (yexiaozhou2003@gmail.com)"
+  $postFormat = "login_email=" + $login_email + "&login_password=" + $login_password + "&format=" + $format + "&lang=" + $lang
+
+  $getIpUrl = "http://members.3322.org/dyndns/getip"
+
+# Defination of functions
+
+  def PostRequest(functionAddr, postContent)
+    http = Net::HTTP.new("dnsapi.cn", 443)
+    http.use_ssl = true
+    headers = {
+        'Content-Type' => 'application/x-www-form-urlencoded',
+        'User-Agent' => $userAgent
+    }
+
+    #puts postContent
+    response = http.post2(functionAddr, postContent, headers)
+
+    #Output response for debug
+    #puts response.code
+    #puts response.message
+    #puts response.body
+
+    return response
+  end
+
+  def GetPublicIPAddr()
+    return open($getIpUrl).read
+  end
+
+  def GetAPIVersion()
+    response = PostRequest("/Info.Version", $postFormat)
+    content = JSON.parse(response.body)
+
+    if(content['status']['code'] == "1")
+      return content['status']['message']
+    else
+      puts "Failed to get API version!"
+    end
+  end
+
+  def GetDomainInfo()
+    response = PostRequest("/Domain.List", $postFormat + "&type=all&offset=0&length=20")
+    content = JSON.parse(response.body)
+    domainInfo = Hash.new
+
+    if(content['status']['code'] == "1")
+      content['domains'].each { |obj|
+        domainInfo[obj['name']] = obj['id']
+      }
+    else
+      puts "Failed to get domain id..."
+    end
+
+    return domainInfo
+  end
+
+  def GetSubDomainIP(domainId, subDomain)
+    response = PostRequest("/Record.List", $postFormat + "&domain_id=" + domainId.to_s() + "&offset=0&length=30")
+    content = JSON.parse(response.body)
+    subDomains = Hash.new
+
+    if(content['status']['code'] == "1")
+      content['records'].each { |obj|
+        subDomains[obj['name']] = obj['id']
+      }
+    else
+      puts "Failed to get sub-domain records..."
+    end
+
+    return subDomains[subDomain]
+  end
+end
--- DynDNS.rb
+++ DynDNS.rb
@@ -1,4 +1,4 @@
-# Dynamic DNS script for DNSPod
+# Dynamic DNS scripts for DNSPod
 # Written by Timothy
 # Blog http://www.xiaozhou.net
 # Create Date: 2012.4.4
@@ -10,6 +10,8 @@ require 'net/http'
 require 'net/https'
 require 'open-uri'
 require 'json'
+require './DNSPodHelper.rb'
+
 
 # Defination of static strings
 
@@ -19,86 +21,16 @@ $login_password = "123456"
 $your_Domain = "ourvps.com"
 $your_SubDomain = "transmission"
 
-$format = "json"
-$lang = "en"
-
-$userAgent = "DynDNS/0.1 (yexiaozhou2003@gmail.com)"
-$postFormat = "login_email=" + $login_email + "&login_password=" + $login_password + "&format=" + $format + "&lang=" + $lang
-
-$getIpUrl = "http://members.3322.org/dyndns/getip"
-
-# Defination of functions
-def PostRequest(functionAddr, postContent)
-  http = Net::HTTP.new("dnsapi.cn", 443)
-  http.use_ssl = true
-  headers = {
-      'Content-Type' => 'application/x-www-form-urlencoded',
-      'User-Agent' => $userAgent
-  }
-
-  #puts postContent
-  response = http.post2(functionAddr, postContent, headers)
-
-  #Output response for debug
-  #puts response.code
-  #puts response.message
-  #puts response.body
-
-  return response
-end
-
-def GetPublicIPAddr()
-  return open($getIpUrl).read
-end
-
-def GetAPIVersion()
-  response = PostRequest("/Info.Version", $postFormat)
-  content = JSON.parse(response.body)
-
-  if(content['status']['code'] == "1")
-    return content['status']['message']
-  else
-    puts "Failed to get API version!"
-  end
-end
-
-def GetDomainInfo()
-  response = PostRequest("/Domain.List", $postFormat + "&type=all&offset=0&length=20")
-  content = JSON.parse(response.body)
-  domainInfo = Hash.new
-
-  if(content['status']['code'] == "1")
-    content['domains'].each { |obj|
-      domainInfo[obj['name']] = obj['id']
-    }
-  else
-    puts "Failed to get domain id..."
-  end
-
-  return domainInfo
-end
-
-def GetSubDomainIP(domainId, subDomain)
-  response = PostRequest("/Record.List", $postFormat + "&domain_id=" + domainId.to_s() + "&offset=0&length=30")
-  content = JSON.parse(response.body)
-  subDomains = Hash.new
-
-  if(content['status']['code'] == "1")
-    content['records'].each { |obj|
-      subDomains[obj['name']] = obj['id']
-    }
-  else
-    puts "Failed to get sub-domain records..."
-  end
-
-  return subDomains[subDomain]
-end
-
 # Execute section
 
-#puts GetPublicIPAddr()
-puts GetAPIVersion()
-result = GetDomainInfo()
-puts result
-result1 = GetSubDomainIP(result[$your_Domain], $your_SubDomain)
-puts result1
+helper = DNSPodHelper.instance
+helper.SetUserInfo('abc@gmail.com', '123')
+
+puts helper.GetPublicIPAddr()
+
+#puts GetAPIVersion()
+#result = GetDomainInfo()
+#puts result
+#result1 = GetSubDomainIP(result[$your_Domain], $your_SubDomain)
+#puts result1
+
